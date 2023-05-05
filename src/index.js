a5d419796ca737e17a66f294d96d5e91434b7fa8
OmkarTerbhai <omkarterbhai@gmail.com>
2023-05-05 09:10:35 +0530

    Created basic node project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { ServerConfig, Logger } = require('./config');
+const express = require('express');
+const apiRoutes = require('./routes');
+const app = express();
+
+app.use('/api', apiRoutes);
+app.listen(ServerConfig.PORT, () => {
+    console.log("Successfully started the server on port :"+ServerConfig.PORT);
+    Logger.info("Successfully started server", {});
+})
